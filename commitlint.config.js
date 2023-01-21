4f923783a70062d37b6461afc3ecf816569d4f07
wwsheng009 <vincentwwsheng@gmail.com>
2023-01-21 22:27:29 +0800

    feature(custom): update new lint-staged

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,13 @@
 module.exports = {
+  ignores: [(commit) => commit.includes('init')],
   extends: ['@commitlint/config-conventional', 'cz'],
   rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
     'type-enum': [
       2,
       'always',
